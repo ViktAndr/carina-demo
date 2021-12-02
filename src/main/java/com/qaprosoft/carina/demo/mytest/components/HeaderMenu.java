2aa4f02498464c65a980483b15c5c1a9b5f6daee
Viktoria Andrienko <viktoriaandrienko@MacBook-Pro-Viktoria.local>
2021-12-02 14:30:05 +0200

    task3

--- src/main/java/com/qaprosoft/carina/demo/mytest/components/HeaderMenu.java
+++ src/main/java/com/qaprosoft/carina/demo/mytest/components/HeaderMenu.java
@@ -8,11 +8,46 @@ import org.openqa.selenium.support.FindBy;
 
 public class HeaderMenu extends AbstractUIObject {
 
-    @FindBy(xpath = "//i[contains(@class, 'head-icon icon-user')]")
-    private ExtendedWebElement logOutButton;
+    @FindBy(xpath = "//button[contains(@type, 'button')]")
+    private ExtendedWebElement headerNavigation;
+
+    @FindBy(xpath = "//button[contains(@class,'lines-button minus')]")
+    private ExtendedWebElement gsmarena;
+
+    @FindBy(id = "topsearch-text")
+    private ExtendedWebElement gsmarenaSearchText;
+
+    @FindBy(id = "quick-search-button")
+    private ExtendedWebElement searchButton;
+
+    @FindBy(xpath = "//i[contains(@class,'head-icon icon-tip-us icomoon-liga')]")
+    private ExtendedWebElement tipIcon;
+
+    @FindBy(xpath = "//i[contains(@class,'head-icon icon-soc-fb2 icomoon-liga')]")
+    private ExtendedWebElement facebookIcon;
+
+    @FindBy(xpath = "//i[contains(@class,'head-icon icon-soc-twitter2 icomoon-liga')]")
+    private ExtendedWebElement twitterIcon;
+
+    @FindBy(xpath = "//i[contains(@class,'head-icon icon-instagram icomoon-liga')]")
+    private ExtendedWebElement instagramIcon;
+
+    @FindBy(xpath = "//i[contains(@class,'head-icon icon-soc-youtube icomoon-liga')]")
+    private ExtendedWebElement youtubeIcon;
+
+    @FindBy(xpath = "//i[contains(@class,'head-icon icon-soc-rss2 icomoon-liga')]")
+    private ExtendedWebElement rssIcon;
+
+    @FindBy(xpath = "//i[contains(@class,'head-icon icon-user-plus')]")
+    private ExtendedWebElement singUpIcon;
 
     @FindBy(id = "login-active")
     private ExtendedWebElement loginIcon;
+
+
+    @FindBy(xpath = "//i[contains(@class, 'head-icon icon-user')]")
+    private ExtendedWebElement logOutButton;
+
 
     public HeaderMenu(WebDriver driver, SearchContext searchContext) {
         super(driver, searchContext);
@@ -25,5 +60,49 @@ public class HeaderMenu extends AbstractUIObject {
 
     public boolean isLogOutButtonPresent() {
         return logOutButton.isPresent();
+    }
+
+    public boolean isGsmarenaButtonPresent() {
+        return gsmarena.isPresent();
+    }
+
+    public boolean isGsmarenaSearchTextPresent() {
+        return gsmarenaSearchText.isPresent();
+    }
+
+    public boolean isSearchButtonPresent() {
+        return searchButton.isPresent();
+    }
+
+    public boolean isTipIconPresent() {
+        return tipIcon.isPresent();
+    }
+
+    public boolean isFacebookIconPresent() {
+        return facebookIcon.isPresent();
+    }
+
+    public boolean isTwitterIconPresent() {
+        return twitterIcon.isPresent();
+    }
+
+    public boolean isInstagramIconPresent() {
+        return instagramIcon.isPresent();
+    }
+
+    public boolean isYoutubeIconPresent() {
+        return youtubeIcon.isPresent();
+    }
+
+    public boolean isRssIconPresent() {
+        return rssIcon.isPresent();
+    }
+
+    public boolean isSingUpIconPresent() {
+        return singUpIcon.isPresent();
+    }
+
+    public boolean isLoginIconPresent() {
+        return loginIcon.isPresent();
     }
 }
